66fc06ec573996e2a4af79ae799f9452c3741103
JadeCathleen <cormier.jade@gmail.com>
2022-03-30 19:10:05 +0200

    add buttons to cards

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :emojis, dependent: :destroy
   has_one_attached :photo
   validates :photo, attached: true
   validates :title, presence: true
