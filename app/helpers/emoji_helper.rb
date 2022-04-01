781c8d3d6e9677a3af792709522cd2963f3fa5a9
JadeCathleen <cormier.jade@gmail.com>
2022-04-01 11:50:29 +0200

    add beg comments + fix size of cards and position of buttons in cards

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -1,9 +1,10 @@
 module EmojiHelper
+  # created an emoji helper method to deal with the color of the button
   def bg_status(emoji)
-    if emoji.nil?
-      return "white"
-    else
-      return "purple"
+    if emoji.nil? # if the user did not select the emoji yet, which means it does not exist yet
+      return "white" # white background and black writing color - cf assets/stylesheets/components/buttons.scss
+    else # if the user already selected the emoji, which means it does exist
+      return "purple" # blue/purple background and white writing color - cf assets/stylesheets/components/buttons.scss
     end
   end
 end
